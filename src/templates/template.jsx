cc6ffe78ebab0bd95e0df8a871411a9ab1453329
stfg.prof <stfg.prof@gmail.com>
2025-09-10 00:02:47 -0300

    porlas

--- src/templates/template.jsx
+++ src/templates/template.jsx
@@ -1,92 +1,95 @@
-// import React from "react";
-// import { graphql } from "gatsby";
-
-// import { MDXProvider } from "@mdx-js/react";
-
-// export default function Tempate({ data: mdx }) {
-//     const { frontmatter,body } = mdx;
-
-//   return (
-//     <>
-//       <h2>{frontmatter.date}</h2>
-//       <MDXProvider>{body}</MDXProvider>
-//     </>
-//   );
-// }
-
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import { MDXProvider } from "@mdx-js/react"
-import { MDXRenderer } from "gatsby-plugin-mdx";
 
-const MyH1 = (props) => <h1 style={{ color: `tomato` }} {...props} />;
-const MyParagraph = (props) => (
-  <p style={{ fontSize: "18px", lineHeight: 1.6 }} {...props} />
-);
+import { MDXProvider } from "@mdx-js/react";
 
-export default function BlogPostTemplate({
-  data: { mdx } ,children
-}) {
-  // const { mdx } = data; // data.mdx holds your post data
-  const { frontmatter,body } = mdx;
+// export default function BlogPostTemplate({  data: { mdx } ,children}){}
+export default function BlogPostTemplate({ data: {mdx} }) {
+    const { frontmatter,body } = mdx;
+    console.log(mdx);
 
-  const components = {
-    h1: MyH1,
-    p: MyParagraph,
-  };
-
-  return (
-    <Layout titulo={frontmatter.title}>
-      <main className=" w-9/12 md:container mx-auto text-justify ">
-        {/* <h2>{frontmatter.date}</h2> */}
-        <>
-          <h1>{frontmatter.title}</h1>
-          <MDXProvider >
-          {body}
-          {children}
-          </MDXProvider>
-{/*
-            <MDXRenderer>
-            {body}
-
-            </MDXRenderer> */}
-
-        </>
-
-        {/* <div          className=" unreset"         dangerouslySetInnerHTML={{ __html: html }}        /> */}
-      </main>
-    </Layout>
-  );
-}
-export const Head = ({ data, pageContext }) => {
   return (
     <>
-      <title>{data.mdx.frontmatter.title}</title>
+<h1>{frontmatter.title}</h1>
 
-      <meta property="og:title" content={data.mdx.frontmatter.title} />
-      <meta
-        property="og:image"
-        content={
-          "https://santitfg.github.io" +
-          data.mdx.frontmatter.image.childImageSharp.gatsbyImageData.images
-            .fallback.src
-        }
-      />
-      <meta
-        property="og:url"
-        content={"https://santitfg.github.io" + data.mdx.fields.slug}
-      />
-      <meta property="og:type" content="post" />
-      <meta
-        name="og:description"
-        content={
-          "post de santi fecha de contenido: " + data.mdx.frontmatter.date
-        }
-      />
+      <MDXProvider>{body}</MDXProvider>
     </>
   );
-};
+}
+
+// import React from "react";
+// import { graphql } from "gatsby";
+// import Layout from "../components/layout";
+// import { MDXProvider } from "@mdx-js/react"
+// import { MDXRenderer } from "gatsby-plugin-mdx";
+
+// const MyH1 = (props) => <h1 style={{ color: `tomato` }} {...props} />;
+// const MyParagraph = (props) => (
+//   <p style={{ fontSize: "18px", lineHeight: 1.6 }} {...props} />
+// );
+
+// export default function BlogPostTemplate({
+//   data: { mdx } ,children
+// }) {
+//   // const { mdx } = data; // data.mdx holds your post data
+//   const { frontmatter,body } = mdx;
+
+//   const components = {
+//     h1: MyH1,
+//     p: MyParagraph,
+//   };
+
+//   return (
+//     <Layout titulo={frontmatter.title}>
+//       <main className=" w-9/12 md:container mx-auto text-justify ">
+//         {/* <h2>{frontmatter.date}</h2> */}
+//         <>
+//           <h1>{frontmatter.title}</h1>
+//           <MDXProvider >
+//           {body}
+//           {children}
+//           </MDXProvider>
+// {/*
+//             <MDXRenderer>
+//             {body}
+
+//             </MDXRenderer> */}
+
+//         </>
+
+//         {/* <div          className=" unreset"         dangerouslySetInnerHTML={{ __html: html }}        /> */}
+//       </main>
+//     </Layout>
+//   );
+// }
+// export const Head = ({ data, pageContext }) => {
+//   return (
+//     <>
+//       <title>{data.mdx.frontmatter.title}</title>
+
+//       <meta property="og:title" content={data.mdx.frontmatter.title} />
+//       <meta
+//         property="og:image"
+//         content={
+//           "https://santitfg.github.io" +
+//           data.mdx.frontmatter.image.childImageSharp.gatsbyImageData.images
+//             .fallback.src
+//         }
+//       />
+//       <meta
+//         property="og:url"
+//         content={"https://santitfg.github.io" + data.mdx.fields.slug}
+//       />
+//       <meta property="og:type" content="post" />
+//       <meta
+//         name="og:description"
+//         content={
+//           "post de santi fecha de contenido: " + data.mdx.frontmatter.date
+//         }
+//       />
+//     </>
+//   );
+// };
 
 export const pageQuery = graphql`
   query ($slug: String!) {
